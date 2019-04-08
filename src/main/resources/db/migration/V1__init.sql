832e060a82d61d08ee3a1357aa164af8c65e5a60
Suresh Kodavati <sureshaws2143@gmail.com>
2019-04-08 13:45:34 -0400

    Update V1__init.sql

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,1 +1,1 @@
-CREATE TABLE users (id bigint NOT NULL, username varchar(100) NOT NULL, first_name varchar(50) NOT NULL, last_name varchar(50) DEFAULT NULL,  PRIMARY KEY (id));
+CREATE TABLE flypoc_users (id bigint NOT NULL, username varchar(100) NOT NULL, first_name varchar(50) NOT NULL, last_name varchar(50) DEFAULT NULL,  PRIMARY KEY (id));
