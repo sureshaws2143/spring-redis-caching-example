e4f24df1ca1da15f32c03886688eb36825835008
Suresh Kodavati <sureshaws2143@gmail.com>
2019-04-08 13:52:08 -0400

    Update V2__testdata.sql

--- src/main/resources/db/migration/V2__testdata.sql
+++ src/main/resources/db/migration/V2__testdata.sql
@@ -1,2 +1,2 @@
 INSERT INTO flypoc_users(id,username, first_name, last_name) VALUES(1,'callicoder', 'Rajeev', 'Singh');
-INSERT INTO flypoc_users(id.username, first_name, last_name) VALUES(2,'flywaytest', 'Flyway', 'Test');
+INSERT INTO flypoc_users(id,username, first_name, last_name) VALUES(2,'flywaytest', 'Flyway', 'Test');
